487c5cf85a9084c393fa786b1ef10c39e9591697
DimOK KryloFF <dimok.krtyloff@gmail.com>
2023-02-13 00:26:49 +0300

    add Randome

--- Exm003_Sum/Program.cs
+++ Exm003_Sum/Program.cs
@@ -3,8 +3,17 @@
 //Console.WriteLine(numberA + numberB);
 
 // дополнительная переменная rezult
-int numberA = 3;
-int numberB = 55;
+//int numberA = 3;
+//int numberB = 55;
+//int rezult = numberA + numberB;
+//Console.WriteLine(rezult);
+
+
+// добавим случайные величины
+int numberA = new Random().Next(1, 10); // 1,2,3,....9
+Console.WriteLine(numberA);
+int numberB = new Random().Next(1, 10);
+Console.WriteLine(numberB);
 int rezult = numberA + numberB;
 Console.WriteLine(rezult);
 
