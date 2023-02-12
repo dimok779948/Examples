69bc5a49804d10a7eeacddcabcdfb17e69035475
DimOK KryloFF <dimok.krtyloff@gmail.com>
2023-02-13 00:04:23 +0300

    add Exm003 Sum

---
+++ Exm003_Sum/Program.cs
@@ -0,0 +1,10 @@
+﻿//int numberA = 3;
+//int numberB = 5;
+//Console.WriteLine(numberA + numberB);
+
+// дополнительная переменная rezult
+int numberA = 3;
+int numberB = 55;
+int rezult = numberA + numberB;
+Console.WriteLine(rezult);
+
