ad6f11a1e6dbb1f12b820aa933e156028f0004d4
DimOK KryloFF <dimok.krtyloff@gmail.com>
2023-02-12 20:07:56 +0300

    add Exm002 Hello User

---
+++ Exm002_HelloUser/Program.cs
@@ -0,0 +1,11 @@
+﻿//это вариант на каждой строке 
+//Console.WriteLine("Введите ваше имя:");
+//string username = Console.ReadLine();
+//Console.WriteLine("Приветик, ");
+//Console.WriteLine(username);
+
+// это в одну строку
+Console.Write("Введите ваше имя:");
+string username = Console.ReadLine();
+Console.Write("Приветик, ");
+Console.Write(username);
