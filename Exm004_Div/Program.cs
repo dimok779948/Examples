580e52b505a029e4a21d68d412fb284727e82ced
DimOK KryloFF <dimok.krtyloff@gmail.com>
2023-02-13 00:15:01 +0300

    add Exm004 Div

---
+++ Exm004_Div/Program.cs
@@ -0,0 +1,9 @@
+﻿//int numberA = 10;
+//int numberB = 5;
+//Console.WriteLine(numberA / numberB);
+// это только для целых чисел
+
+// это уже для дробных
+Double numberA = 12;
+Double numberB = 5;
+Console.WriteLine(numberA / numberB);
