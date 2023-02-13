0ffb518f62e6d50193603fb6416c91be4cd78062
DimOK KryloFF <dimok.krtyloff@gmail.com>
2023-02-13 20:50:58 +0300

    add Exm006 Base

---
+++ Exm006_Base/Program.cs
@@ -0,0 +1,16 @@
+﻿int a = 1;
+int b = 2;
+int c = 6;
+int d = 8;
+int e = 4;
+
+int max = a; // вводим переменную макс, куда кладем значение А, и потом поочередно сравниваем
+
+if (a > max) max = a;
+if (b > max) max = b;
+if (c > max) max = c;
+if (d > max) max = d;
+if (e > max) max = e;
+
+Console.Write("max = ");
+Console.WriteLine(max);
